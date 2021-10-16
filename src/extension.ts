fa9dda013a7892730dc2dff60a842aaf412066f0
tchojnacki <tomaszchojnacki2001@gmail.com>
2021-10-16 21:23:14 +0200

    "Run in REPL" status bar button

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,30 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('ocaml-scala-run.runOCaml', () => {
-		vscode.window.showInformationMessage('Hello World from ocaml-scala-run!');
-	}));
+let statusBarItem: vscode.StatusBarItem;
 
-	context.subscriptions.push(vscode.commands.registerCommand('ocaml-scala-run.runScala', () => {
-		vscode.window.showInformationMessage('Hello World from ocaml-scala-run!');
-	}));
+export function activate(context: vscode.ExtensionContext) {
+	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10);
+	statusBarItem.text = '$(debug-start) Run in REPL';
+	statusBarItem.command = 'ocaml-scala-run.runInRepl';
+
+	context.subscriptions.push(vscode.commands.registerCommand('ocaml-scala-run.runInRepl', runInReplCommand));
+
+	context.subscriptions.push(statusBarItem);
+
+	vscode.workspace.onDidOpenTextDocument(document => updateStatusBarItem(document.languageId));
+	vscode.window.onDidChangeActiveTextEditor(editor => updateStatusBarItem(editor?.document.languageId ?? ''));
+
+	updateStatusBarItem(vscode.window.activeTextEditor?.document.languageId ?? '');
 }
 
-export function deactivate() { }
+function updateStatusBarItem(languageId: string) {
+	if (languageId === 'ocaml' || languageId === 'scala') {
+		statusBarItem.show();
+	} else {
+		statusBarItem.hide();
+	}
+}
+
+function runInReplCommand() {
+	vscode.window.showInformationMessage('Hello World from ocaml-scala-run!');
+}
